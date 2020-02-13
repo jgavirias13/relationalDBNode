4677b8b02dcdb64b2a7320944bd641ef30f6686b
Juan Pablo Gaviria Salazar <jgavirias13@gmail.com>
2020-02-12 21:35:53 -0500

    Cambio en la semilla formato de fecha incorrecta Appjs Metodo de create y de read

--- app.js
+++ app.js
@@ -5,37 +5,61 @@
         opcion:
             --create
         argumentos:
-            --firstname=VALOR
+            --CLAVE=VALOR
 */
 
-const CRUD = require('./helpers/crud');
-const db = require('./models');
+const CRUD = require("./helpers/crud");
+const db = require("./models");
 
 const params = process.argv;
 
 //Evaluacion de que existan mas de dos parametros para su ejecucion
 
-if(params <= 2){
-    console.log("Nada para ejecutar");
-    process.exit(0);
+if (params <= 2) {
+  console.log("Nada para ejecutar");
+  process.exit(0);
 }
 
 //Se toma solo los argumentos a partir de la segunda posicion
 const args = params.slice(2);
 
-const command = args[0].split(':')[0].substring(2);
-const entity = args[0].split(':')[1];
+const command = args[0].split(":")[0].substring(2);
+const entity = args[0].split(":")[1];
 
-switch(command){
-    case CRUD.CREATE:
-        console.log('Operacion de creacion');
-        break;
-    case CRUD.READ:
-        break;
-    case CRUD.UPDATE:
-        break;
-    case CRUD.DELETE:
-        break;
-    default:
-        console.log('Operacion no reconocida');
+switch (command) {
+  case CRUD.CREATE:
+    const data = {};
+    //Toma de argumentos para la operacion
+    const argumentos = args.slice(1);
+    argumentos.forEach(arg => {
+      const argumento = arg.split("=");
+      data[argumento[0].substring(2)] = argumento[1];
+    });
+
+    db[entity]
+      .create(data)
+      .then(() => {
+        console.log("Elemento creado");
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    break;
+  case CRUD.READ:
+    db[entity].findAll()
+      .then((entidades) => {
+          console.log(entidades);
+      })
+      .catch((err) => {
+          console.log(err);
+      })
+    break;
+  case CRUD.UPDATE:
+    break;
+  case CRUD.DELETE:
+    break;
+  default:
+    console.log("Operacion no reconocida");
 }
--- seeders/20200213015906-seedContact.js
+++ seeders/20200213015906-seedContact.js
@@ -8,35 +8,29 @@ module.exports = {
         lastname: "Gaviria",
         phone: "2060856",
         email: "jgavirias13@gmail.com",
-        createdAt: new Date().toDateString(),
-        updatedAt: new Date().toDateString()
+        createdAt: new Date().toUTCString(),
+        updatedAt: new Date().toUTCString()
       },
       {
         firstname: "Miguel",
         lastname: "Gaviria",
         phone: "34342929",
         email: "elmigue@hotmail.com",
-        createdAt: new Date().toDateString(),
-        updatedAt: new Date().toDateString()
+        createdAt: new Date().toUTCString(),
+        updatedAt: new Date().toUTCString()
       },
       {
         firstname: "Carolina",
         lastname: "Gaviria",
         phone: "3938484",
         email: "cgavir25@gmail.com",
-        createdAt: new Date().toDateString(),
-        updatedAt: new Date().toDateString()
+        createdAt: new Date().toUTCString(),
+        updatedAt: new Date().toUTCString()
       }
     ]);
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete('Contacts', null, {});
   }
 };

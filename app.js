566996ce9459113ab301f505e49e9abdcd3c2801
Juan Pablo Gaviria Salazar <jgavirias13@gmail.com>
2020-02-12 21:18:11 -0500

    Toma de parametros Creacion de switch para distincion de operaciones

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
+/*
+    Programa para realizar CRUD por linea de comandos
+    Uso:
+        node app.js [opcion] [argumentos]
+        opcion:
+            --create
+        argumentos:
+            --firstname=VALOR
+*/
+
 const CRUD = require('./helpers/crud');
 const db = require('./models');
 
 const params = process.argv;
-console.log(params);
+
+//Evaluacion de que existan mas de dos parametros para su ejecucion
+
+if(params <= 2){
+    console.log("Nada para ejecutar");
+    process.exit(0);
+}
+
+//Se toma solo los argumentos a partir de la segunda posicion
+const args = params.slice(2);
+
+const command = args[0].split(':')[0].substring(2);
+const entity = args[0].split(':')[1];
+
+switch(command){
+    case CRUD.CREATE:
+        console.log('Operacion de creacion');
+        break;
+    case CRUD.READ:
+        break;
+    case CRUD.UPDATE:
+        break;
+    case CRUD.DELETE:
+        break;
+    default:
+        console.log('Operacion no reconocida');
+}
